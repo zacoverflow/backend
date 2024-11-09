12d548e00b1dd66fbd9d40be32e62d62b5943a73
zacoverflow <samuelstapleton291204@gmail.com>
2024-11-10 00:04:17 +1100

    fix

--- test.py
+++ test.py
@@ -14,7 +14,12 @@ NSW_TOLL_API_KEY = os.environ.get("NSW_TOLL_API_KEY")
 @app.route('/get-fare', methods=['GET'])
 async def get_toll_cost():
     """Calculate toll costs for a route using the toll calculator API."""
-    polyline = request.args.get('route')
+    data = await request.get_json()  # Retrieve JSON body data
+    polyline = data.get('route')
+    duration = data.get('duration')
+    distance = data.get('distance')
+    print(duration)
+    print(distance)
     headers = {
         'Content-Type': 'application/json',
         'accept': 'application/json',
